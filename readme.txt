8c61641c8aa9f61845dc0321105d1e3d70e55a43
sebat <sebat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-28 07:01:28 +0000

    Adding first version of my plugin
    git-svn-id: http://plugins.svn.wordpress.org/show-qr-url/trunk@222273 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,52 @@
+=== Plugin Name ===
+Contributors: sebat
+Donate link: http://sebastian.thiele.me/
+Tags: widget, sidebar, QR-Code, mobile
+Requires at least: 2.0
+Tested up to: 2.9.2
+Stable tag: 1.0
+
+Diese Plugin baut ein Sidebar-Widget ein, in dem ein in der größe anpassbarer QR Code angezeigt wird, der auf die aktuelle URL verweist.
+
+== Description ==
+
+Dieses Plugin stellt ein Sidebar widmet zur Verfügung, welches die aktuelle URL des Artikels oder der Seite anzeigt.
+Die größe der Grafik ist frei anpassbar und passt sich somit an jede Sidebar an. Es besteht auch die Möglichkeit einen beschreibenden Text unterhalb der Grafik anzugeben um den Benutzern klar zu machen, was es mit der Grafik auf sich hat.
+
+Dabei wird die Google Chart-Api verwendet.
+
+* Die Bilder sind leicht speicherbar und können so, zum Beispiel in Präsentationen, eingebaut werden.
+* Sollte sich ein Benutzer dazu entschließen, einen Artikel auszudrucken, kann er/ sie die Ursprungsseite schnell und einfach durch Abfotografieren des Codes wieder öffnen.
+* Ein Benutzer, der in Eile ist, kann die Seite in einem mobilen Entgerät öffnen und unterwegs lesen.
+* Es ist eine relativ neue und inovative Technik
+
+== Installation ==
+
+1. Die Datei showQRURL.php in das Verzeichnis `/wp-content/plugins/` uploaden
+1. Das Plugin im Adminmenü under  'Plugins' aktivieren
+1. In das Menü `Design` gehen und unter `Widmest` das Widmet in die Sidebar ziehen und die Optionsfelder ausfüllen
+
+== Frequently Asked Questions ==
+
+= Was ist ein QR-Code =
+
+Der QR-Code ist ein zweidimensionaler Barcode, der von (zum Beispiel) Handys abfotografiert werden kann, entschlüsselt wird und in diesem Fall den Benutzer dann auf die URL der aktuellen Seite leitet.
+
+== Screenshots ==
+
+1. Einstellungen im Backend für das Sidebar Widget
+2. Beispielanzeige, wie es im Frontend aussehen kann.
+
+== Changelog ==
+
+= 1.0 =
+* Einbau der der Sidebar
+* Größenanpassung
+* Individueller Titel
+* Individueller Beschreibungstext
+
+== Upgrade Notice ==
+
+= 1.0 =
+Startversion
+
---
+++ showQRURL.php
@@ -0,0 +1,81 @@
+<?php
+/*
+Plugin Name: Show QR URL
+Plugin URI: http://sebastian.thiele.me/projekte/wordpress-plugin-show-qr-url
+Description: This Plugin shows a QR-Code with the encoded URL of the corent Page. Useing Google Shart API
+Author: Sebastian Thiele
+Author URI: htt://sebastian.thiele.me
+Version: 1.0
+*/
+
+function getQRURL($size)
+{
+	return 'http://chart.apis.google.com/chart?chs='.$size.'x'.$size.'&cht=qr&chl='.urlencode ("http://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]).'&choe=UTF-8';;
+}
+
+function widget_showQRURL($args) {
+	
+	extract($args);
+	
+	$options = get_option("widget_showQRURL");
+	if(!is_array($options)){
+			$options[title] = "QR URL";
+			$options[size] = 200;
+			$options[text] = "Das ist ein so genannter QR Code. Wenn ihr ein Handy mit einer Kamera und ein installiertes Leseprogramm habt, könnt ihr den Code einscannen und werdet irgendwo hingeleitet";
+	}
+	
+	
+	echo $before_widget;
+	echo $before_title;
+	echo $options[title];
+	echo $after_title;
+	echo "<div style=\"text-align:center\">";
+	echo "<img src=\"".getQRURL($options[size])."\" alt=\"QR Code für diese Seite\" />";
+	echo "</div>";
+	echo "<div class=\"showQRURLtext\">".$options[text]."</div>";
+	echo $after_widget;
+}
+
+function showQRURL_control() {
+	
+	$options = get_option("widget_showQRURL");
+	
+	
+	if($_POST[showQRURLsubmit]) {
+		$options[title] = htmlspecialchars($_POST[showQRURLtitle]);
+		$options[size] = htmlspecialchars($_POST[showQRURLsize]);
+		$options[text] = htmlspecialchars($_POST[showQRURLtext]);
+		update_option("widget_showQRURL", $options);
+	}
+	
+	if(!is_array($options)){
+			$options[title] = "QR URL";
+			$options[size] = 200;
+			$options[text] = "Das ist ein so genannter QR Code. Wenn ihr ein Handy mit einer Kamera und ein installiertes Leseprogramm habt, könnt ihr den Code einscannen und werdet irgendwo hingeleitet";
+
+	}
+	
+	
+	echo "
+	<p>
+		<label for=\"showQRURLtitle\">&Uuml;berschrift:</label>
+		<input type=\"text\" id=\"showQRURLtitle\" name=\"showQRURLtitle\" value=\"$options[title]\" />
+	</p>
+	<p>
+		<label for=\"showQRURLsize\">Breite und H&ouml;he:</label>
+		<input type=\"text\" id=\"showQRURLsize\" name=\"showQRURLsize\" value=\"$options[size]\" />
+		<input type=\"hidden\" name=\"showQRURLsubmit\" value=\"submit\" />
+	</p>
+	<p>
+		<label for\"showQRURLtext\">Text unter dem QR Code:</label>
+		<textarea id=\"showQRURLtext\" name=\"showQRURLtext\" style=\"width:100%\">$options[text]</textarea>
+	</p>
+	";
+}
+
+function showQRURL_init(){
+	register_sidebar_widget("Show QR URL", "widget_showQRURL");
+	register_widget_control("Show QR URL", "showQRURL_control", 300, 200);
+}
+add_action("plugins_loaded", "showQRURL_init");
+?>
