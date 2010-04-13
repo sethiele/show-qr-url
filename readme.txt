fdfee49b87722cc41b7cb9642629420d86e1bbdb
sebat <sebat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-13 14:09:11 +0000

    Adding a new Function to generate QRs in Theme and new Options
    git-svn-id: http://plugins.svn.wordpress.org/show-qr-url/trunk@228393 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -4,53 +4,71 @@ Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_i
 Tags: widget, sidebar, QR-Code, mobile
 Requires at least: 2.0
 Tested up to: 2.9.2
-Stable tag: 1.1
+Stable tag: 1.2
 Author: Sebastian Thiele
 Author URI: htt://sebastian.thiele.me
 
-Diese Plugin baut ein Sidebar-Widget ein, in dem ein in der größe anpassbarer QR Code angezeigt wird, der auf die aktuelle URL verweist.
+Diese Plugin baut ein Sidebar-Widget ein, in dem ein in der groessee anpassbarer QR Code angezeigt wird, der auf die aktuelle URL verweist.
 
 == Description ==
 
-Dieses Plugin stellt ein Sidebar widmet zur Verfügung, welches die aktuelle URL des Artikels oder der Seite anzeigt.
-Die größe der Grafik ist frei anpassbar und passt sich somit an jede Sidebar an. Es besteht auch die Möglichkeit einen beschreibenden Text unterhalb der Grafik anzugeben um den Benutzern klar zu machen, was es mit der Grafik auf sich hat.
+Dieses Plugin stellt ein Sidebar widget zur Verfuegung, welches die aktuelle URL des Artikels oder der Seite anzeigt.
+Die groessee der Grafik ist frei anpassbar und passt sich somit an jede Sidebar an. Es besteht auch die Moeglichkeit einen beschreibenden Text unterhalb der Grafik anzugeben um den Benutzern klar zu machen, was es mit der Grafik auf sich hat.
 
 Dabei wird die Google Chart-Api verwendet.
 
-* Die Bilder sind leicht speicherbar und können so, zum Beispiel in Präsentationen, eingebaut werden.
-* Sollte sich ein Benutzer dazu entschließen, einen Artikel auszudrucken, kann er/ sie die Ursprungsseite schnell und einfach durch Abfotografieren des Codes wieder öffnen.
-* Ein Benutzer, der in Eile ist, kann die Seite in einem mobilen Entgerät öffnen und unterwegs lesen.
+* Die Bilder sind leicht speicherbar und koennen so, zum Beispiel in Praesentationen, eingebaut werden.
+* Sollte sich ein Benutzer dazu entschliessen, einen Artikel auszudrucken, kann er/ sie die Ursprungsseite schnell und einfach durch Abfotografieren des Codes wieder oeffnen.
+* Ein Benutzer, der in Eile ist, kann die Seite in einem mobilen Entgerart oeffnen und unterwegs lesen.
 * Es ist eine relativ neue und inovative Technik
 
 == Installation ==
 
 1. Die Datei showQRURL.php in das Verzeichnis `/wp-content/plugins/` uploaden
-1. Das Plugin im Adminmenü under  'Plugins' aktivieren
-1. In das Menü `Design` gehen und unter `Widmest` das Widmet in die Sidebar ziehen und die Optionsfelder ausfüllen
+1. Das Plugin im Adminmenue under  'Plugins' aktivieren
+1. In das Menue `Design` gehen und unter `Widmest` das Widmet in die Sidebar ziehen und die Optionsfelder ausfuellen
 
 == Frequently Asked Questions ==
 
 = Was ist ein QR-Code =
 
-Der QR-Code ist ein zweidimensionaler Barcode, der von (zum Beispiel) Handys abfotografiert werden kann, entschlüsselt wird und in diesem Fall den Benutzer dann auf die URL der aktuellen Seite leitet.
+Der QR-Code ist ein zweidimensionaler Barcode, der von (zum Beispiel) Handys abfotografiert werden kann, entschl‚àö¬∫sselt wird und in diesem Fall den Benutzer dann auf die URL der aktuellen Seite leitet.
+
+= Kann ich auch andere Dinge als QR-Code darstellen? =
+Ja. Es gibt zwei Funktionen, die dies zur verfuegung stellen: 
+`getQRURL($size)` diese Funktion erwartet als Parameter nur eine groesse in Pixel z.B. `getQRURL(120)` Gibt einen QR Code der aktuellen Seite mit den Ma√üen 120px x 120px aus
+`getQR($encode, $size)` diese Funktion erwartet einen String, der umgewandelt werden soll und die groesse.
+Beide Funktionen koennen im Theme verwendet werden z.B.
+`<img src="<?php echo getQRURL($size); ?>" alt="xy" />`
+
 
 == Screenshots ==
 
-1. Einstellungen im Backend für das Sidebar Widget
+1. Einstellungen im Backend fuer das Sidebar Widget
 2. Beispielanzeige, wie es im Frontend aussehen kann.
 
 == Changelog ==
 
+= 1.2 =
+* Code verbessert
+* Neue Funktion hinzugefuegt `getQR($encode, $size)`
+* Auswaehlbar, auf welchen Seiten die GRafik angezeigt werden soll (fuer ein Sidbar Themes)
+* Einbau der Moeglichkeit auf die Entwicklerseite zu verlinken, um die Verbreitung des Plugins zu verbessern
+
+
 = 1.1 =
-* Werte aus dem Backend können jetzt auch HTML enthalten.
+* Werte aus dem Backend koennen jetzt auch HTML enthalten.
 
 = 1.0 =
 * Einbau der der Sidebar
-* Größenanpassung
+* Groessenanpassung
 * Individueller Titel
 * Individueller Beschreibungstext
 
 == Upgrade Notice ==
+
+= 1.2 =
+* Im Widget Menue nachschauen, welche neuen Einstellungsmoeglichkeiten es gibt.
 
 = 1.1 =
 * HTML speicherbar
--- showQRURL.php
+++ showQRURL.php
@@ -8,11 +8,19 @@ Author URI: htt://sebastian.thiele.me
 Version: 1.1
 */
 
+// Einfache ausgabe der aktuellen URL
 function getQRURL($size)
 {
-	return 'http://chart.apis.google.com/chart?chs='.$size.'x'.$size.'&cht=qr&chl='.urlencode ("http://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]).'&choe=UTF-8';;
+	return getQR("http://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"], $size);
 }
 
+// Erweiterte Ausgabe von beliebigen Text
+function getQR($encode, $size) {
+	return 'http://chart.apis.google.com/chart?chs='.$size.'x'.$size.'&cht=qr&chl='.urlencode ($encode).'&choe=UTF-8';
+}
+
+
+// Zeigt das Widget an
 function widget_showQRURL($args) {
 	
 	extract($args);
@@ -21,19 +29,39 @@ function widget_showQRURL($args) {
 	if(!is_array($options)){
 			$options[title] = "QR URL";
 			$options[size] = 200;
-			$options[text] = "Das ist ein so genannter QR Code. Wenn ihr ein Handy mit einer Kamera und ein installiertes Leseprogramm habt, könnt ihr den Code einscannen und werdet irgendwo hingeleitet";
+			$options[text] = "Das ist ein so genannter QR Code. Wenn ihr ein Handy mit einer Kamera und ein installiertes Leseprogramm habt, koennt ihr den Code einscannen und werdet irgendwo hingeleitet";
+	}
+	if($options[showQRURLversion] < 1.2) {
+		$options[showQRURLhome] = "checked";
+		$options[showQRURLsingle] = "checked";
+		$options[showQRURLpage] = "checked";
+		$options[showQRURLcategory] = "checked";
+		$options[showQRURLtag] = "checked";
+		$options[showQRURLdate] = "checked";
 	}
 	
+	if (is_home() && $options[showQRURLhome] == 'checked') $showQR = true;	elseif (is_single() && $options[showQRURLsingle] == 'checked') $showQR = true;
+	elseif (is_page() && $options[showQRURLpage] == 'checked') $showQR = true;
+	elseif (is_category() && $options[showQRURLcategory] == 'checked') $showQR = true;
+	elseif (is_tag() && $options[showQRURLtag] == 'checked') $showQR = true;
+	elseif (is_date() && $options[showQRURLdate] == 'checked') $showQR = true;
+	else {$showQR = true;}
 	
-	echo $before_widget;
-	echo $before_title;
-	echo $options[title];
-	echo $after_title;
-	echo "<div style=\"text-align:center\">";
-	echo "<img src=\"".getQRURL($options[size])."\" alt=\"QR Code für diese Seite\" />";
-	echo "</div>";
-	echo "<div class=\"showQRURLtext\">".$options[text]."</div>";
-	echo $after_widget;
+	echo "<!-- QR Widtget von Sebastian Thiele - http://sebastian.thiele.me -->";
+	if($showQR)
+	{
+		echo $before_widget;
+		echo $before_title;
+		echo $options[title];
+		echo $after_title;
+		echo "<div style=\"text-align:center\">";
+		echo "<img src=\"".getQRURL($options[size])."\" alt=\"QR Code fuer diese Seite\" />";
+		echo "</div>";
+		echo "<div class=\"showQRURLtext\">".$options[text]."</div>";
+		if($options[showQRURLsupport]) echo "<div class=\"showQRURLsupport\" style=\"text-align:center\">QR-Widget v".$options[showQRURLversion]." by <a href=\"http://sebastian.thiele.me/projekte/wordpress-plugin-show-qr-url\">Sebastian Thiele</a></div>";
+		echo $after_widget;
+	}
+	echo "<!-- QR Widtget von Sebastian Thiele - http://sebastian.thiele.me -->";
 }
 
 function showQRURL_control() {
@@ -42,17 +70,32 @@ function showQRURL_control() {
 	
 	
 	if($_POST[showQRURLsubmit]) {
+		$options[showQRURLversion] = 1.2;
 		$options[title] = $_POST[showQRURLtitle];
 		$options[size] = htmlspecialchars($_POST[showQRURLsize]);
 		$options[text] = $_POST[showQRURLtext];
+		$options[showQRURLhome] = $_POST[showQRURLhome];		$options[showQRURLsingle] = $_POST[showQRURLsingle];
+		$options[showQRURLpage] = $_POST[showQRURLpage];
+		$options[showQRURLcategory] = $_POST[showQRURLcategory];
+		$options[showQRURLtag] = $_POST[showQRURLtag];
+		$options[showQRURLdate] = $_POST[showQRURLdate];
+		$options[showQRURLsupport] = $_POST[showQRURLsupport];
 		update_option("widget_showQRURL", $options);
 	}
 	
 	if(!is_array($options)){
 			$options[title] = "QR URL";
 			$options[size] = 200;
-			$options[text] = "Das ist ein so genannter QR Code. Wenn ihr ein Handy mit einer Kamera und ein installiertes Leseprogramm habt, könnt ihr den Code einscannen und werdet irgendwo hingeleitet";
-
+			$options[text] = "Das ist ein so genannter QR Code. Wenn ihr ein Handy mit einer Kamera und ein installiertes Leseprogramm habt, koennt ihr den Code einscannen und werdet irgendwo hingeleitet";
+	}
+	//Veränderungen im Backend
+	if($options[showQRURLversion] < 1.2) {
+		$options[showQRURLhome] = "checked";
+		$options[showQRURLsingle] = "checked";
+		$options[showQRURLpage] = "checked";
+		$options[showQRURLcategory] = "checked";
+		$options[showQRURLtag] = "checked";
+		$options[showQRURLdate] = "checked";
 	}
 	
 	
@@ -67,9 +110,46 @@ function showQRURL_control() {
 		<input type=\"hidden\" name=\"showQRURLsubmit\" value=\"submit\" />
 	</p>
 	<p>
-		<label for\"showQRURLtext\">Text unter dem QR Code:</label>
+		<label for=\"showQRURLtext\">Text unter dem QR Code:</label>
 		<textarea id=\"showQRURLtext\" name=\"showQRURLtext\" style=\"width:100%\">$options[text]</textarea>
 	</p>
+	<hr />
+	<p>
+		<b>Anzeigeoptionen</b>
+	</p>
+	<p>
+		<label for=\"showQRURLhome\">Anzeigen auf Blog&uuml;bersichtsseite</label>
+		<input type=\"checkbox\" name=\"showQRURLhome\" value=\"checked\" $options[showQRURLhome] />
+	</p>
+	<p>
+		<label for=\"showQRURLsingle\">Anzeigen auf Single-Seiten</label>
+		<input type=\"checkbox\" name=\"showQRURLsingle\" value=\"checked\" $options[showQRURLsingle] />
+	</p>
+	<p>
+		<label for=\"showQRURLpage\">Anzeigen auf Seiten</label>
+		<input type=\"checkbox\" name=\"showQRURLpage\" value=\"checked\" $options[showQRURLpage] />
+	</p>
+	<p>
+		<label for=\"showQRURLcategory\">Anzeigen auf &Uuml;bersichtsseiten f&uuml;r Kategorien</label>
+		<input type=\"checkbox\" name=\"showQRURLcategory\" value=\"checked\" $options[showQRURLcategory] />
+	</p>
+	<p>
+		<label for=\"showQRURLtag\">Anzeigen auf &Uuml;bersichtsseiten f&uuml;r Tags</label>
+		<input type=\"checkbox\" name=\"showQRURLtag\" value=\"checked\" $options[showQRURLtag] />
+	</p>
+	<p>
+		<label for=\"showQRURLdate\">Anzeigen auf &Uuml;bersichtsseiten f&uuml;r Datum</label>
+		<input type=\"checkbox\" name=\"showQRURLdate\" value=\"checked\" $options[showQRURLdate] />
+	</p>
+	<hr />
+	<p>
+		<b>Sonstiges</b>
+	</p>
+	<p>
+		<label for=\"showQRURLsupport\">Link auf Entwicklerseite einbinden</label>
+		<input type=\"checkbox\" name=\"showQRURLsupport\" value=\"checked\" $options[showQRURLsupport] />
+	</p>
+	
 	";
 }
 
